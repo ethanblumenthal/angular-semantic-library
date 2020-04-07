cd4e20fa178e40185730d3aef4d12141110bfe2e
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-04-07 08:40:42 -0500

    Created shared module with divider component.

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [ElementsHomeComponent],
-  imports: [CommonModule, ElementsRoutingModule],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
 })
 export class ElementsModule {}
